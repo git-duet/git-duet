45f1612ff051e19e291600c560e1c819c3b91981
Jesse Szwedko <jesse.szwedko@example.com>
2015-05-25 17:09:08 -0700

    Add git-duet-install-hook

---
+++ src/github.com/jszwedko/go-git-duet/git-duet-install-hook/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	"code.google.com/p/getopt"
+)
+
+const hook = `
+#!/bin/bash
+exec git duet-pre-commit "$@"
+`
+
+func main() {
+	var (
+		quiet = getopt.BoolLong("quiet", 'q', "Silence output")
+		help  = getopt.BoolLong("help", 'h', "Help")
+	)
+
+	getopt.Parse()
+
+	if *help {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
+	output := new(bytes.Buffer)
+	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
+	cmd.Stdout = output
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	hookPath := path.Join(strings.TrimSpace(output.String()), ".git", "hooks", "pre-commit")
+
+	hookFile, err := os.OpenFile(hookPath, os.O_CREATE|os.O_EXCL|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer hookFile.Close()
+
+	if _, err = hookFile.WriteString(hook); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if !*quiet {
+		fmt.Printf("git-duet-install-hook: Installed hook to %s\n", hookPath)
+	}
+}
--- test/git-duet-install-hooks.bats
+++ test/git-duet-install-hooks.bats
@@ -3,15 +3,19 @@
 load test_helper
 
 @test "writes the hook to the pre-commit hook file" {
-  skip "TODO"
-  git duet-install-hook -q
+  run git duet-install-hook -q
   assert_success
   [ -f .git/hooks/pre-commit ]
 }
 
 @test "makes the pre-commit hook executable" {
-  skip "TODO"
-  git duet-install-hook -q
+  run git duet-install-hook -q
   assert_success
   [ -x .git/hooks/pre-commit ]
+}
+
+@test "does not overwrite existing file" {
+  touch .git/hooks/pre-commit
+  run git duet-install-hook -q
+  assert_failure
 }
