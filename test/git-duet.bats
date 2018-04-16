git-duet/git-duet

commit f3406f6dd262584d0fc89d9843be6f93e48f8a3f

Add top level .git-authors configuration

configuration.go
@@ -2,8 +2,10 @@ package duet
 
 import (
 	"os"
+	"os/exec"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -24,8 +26,11 @@ type Configuration struct {
 func NewConfiguration() (config *Configuration, err error) {
 	config = &Configuration{
 		Namespace:   getenvDefault("GIT_DUET_CONFIG_NAMESPACE", "duet.env"),
-		PairsFile:   getenvDefault("GIT_DUET_AUTHORS_FILE", path.Join(os.Getenv("HOME"), ".git-authors")),
 		EmailLookup: os.Getenv("GIT_DUET_EMAIL_LOOKUP_COMMAND"),
+	}
+
+	if config.PairsFile, err = getPairsFile(); err != nil {
+		return nil, err
 	}
 
 	cutoff, err := strconv.Atoi(getenvDefault("'GIT_DUET_SECONDS_AGO_STALE'", "1200"))
@@ -48,6 +53,27 @@ func NewConfiguration() (config *Configuration, err error) {
 	config.StaleCutoff = time.Duration(cutoff) * time.Second
 
 	return config, nil
+}
+
+func getPairsFile() (value string, err error) {
+
+	authorsFile := ".git-authors"
+
+	if os.Getenv("GIT_DUET_AUTHORS_FILE") != "" {
+		return os.Getenv("GIT_DUET_AUTHORS_FILE"), nil
+	}
+
+	gitDirectory, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
+	if err != nil {
+		return "", err
+	}
+
+	gitDirectoryAuthors := path.Join(strings.TrimSpace(string(gitDirectory)), authorsFile)
+	if _, err := os.Stat(gitDirectoryAuthors); err == nil {
+		return gitDirectoryAuthors, nil
+	}
+
+	return path.Join(os.Getenv("HOME"), authorsFile), nil
 }
 
 func getenvDefault(key, defaultValue string) (value string) {

test/git-duet.bats
@@ -174,3 +174,17 @@ load test_helper
   assert_line "GIT_COMMITTER_NAME='Frances Bar'"
   assert_line "GIT_COMMITTER_EMAIL='f.bar@hamster.info.local'"
 }
+
+@test "respects git root level .git-authors configuration" {
+  setup_root_git_author
+  mkdir new_dir
+  cd new_dir
+
+  git duet -q dj fc
+  run git duet
+
+  assert_line "GIT_AUTHOR_NAME='Dane Joe'"
+  assert_line "GIT_AUTHOR_EMAIL='dane@bananas.biz.local'"
+  assert_line "GIT_COMMITTER_NAME='Frances Car'"
+  assert_line "GIT_COMMITTER_EMAIL='f.car@banana.info.local'"
+}

test/git-solo.bats
@@ -112,6 +112,7 @@ load test_helper
   run git config --global "$GIT_DUET_CONFIG_NAMESPACE.git-committer-email"
   assert_success ""
 }
+
 @test "reads configuration globally" {
   git solo -g -q jd
   git solo fb
@@ -155,4 +156,14 @@ load test_helper
   echo "output $output"
   echo "expected output $expected_output"
   assert_line "$expected_output"
+}
+
+@test "respects git root level .git-authors configuration" {
+  setup_root_git_author
+
+  GIT_DUET_SET_GIT_USER_CONFIG=1 git solo -q fc
+  run git config "user.name"
+  assert_success 'Frances Car'
+  run git config "user.email"
+  assert_success 'f.car@banana.info.local'
 }

test/test_helper.bash
@@ -81,6 +81,25 @@ create_branch_commit() {
   git checkout master
 }
 
+setup_root_git_author() {
+  unset GIT_DUET_AUTHORS_FILE
+  cat > ".git-authors" <<EOF
+---
+authors:
+  dj: Dane Joe
+  fc: Frances Car
+
+email:
+  domain: banana.info.local
+
+email_addresses:
+  dj: dane@bananas.biz.local
+  fc: f.car@banana.info.local
+EOF
+  mkdir new_dir
+  cd new_dir
+}
+
 assert_head_is_merge () {
     msha=$(git rev-list --merges HEAD~1..HEAD)
     [ -z "$msha" ] && return 1

commit efbb6fb382cd1436bfd8870cf28fbc4d80cfe85a

Fix error when running git-(solo|duet) outside of a git repo
Should fallback to using the default .git-authors location.

This bug was introduced by f3406f6dd262584d0fc89d9843be6f93e48f8a3f
which added the ability to read from a repository level `.git-authors`
file which inadvertantly introduced a requirement that git-duet and
git-solo be run from within a git repository when GIT_DUET_AUTHORS_FILE
is not set. This should not be required when manipulating or reading
globally set `git-duet` configuration.

Fixes #61

configuration.go
@@ -1,6 +1,7 @@
 package duet
 
 import (
+	"bytes"
 	"os"
 	"os/exec"
 	"path"
@@ -65,16 +66,19 @@ func NewConfiguration() (config *Configuration, err error) {
 }
 
 func getPairsFile() (value string, err error) {
-
 	authorsFile := ".git-authors"
+	defaultAuthorsFile := path.Join(os.Getenv("HOME"), authorsFile)
 
 	if os.Getenv("GIT_DUET_AUTHORS_FILE") != "" {
 		return os.Getenv("GIT_DUET_AUTHORS_FILE"), nil
 	}
 
-	gitDirectory, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
+	gitDirectory, err := exec.Command("git", "rev-parse", "--show-toplevel").CombinedOutput()
 	if err != nil {
-		return "", err
+		if !bytes.Contains(gitDirectory, []byte("Not a git repository")) {
+			return "", err
+		}
+		return defaultAuthorsFile, nil
 	}
 
 	gitDirectoryAuthors := path.Join(strings.TrimSpace(string(gitDirectory)), authorsFile)
@@ -82,7 +86,7 @@ func getPairsFile() (value string, err error) {
 		return gitDirectoryAuthors, nil
 	}
 
-	return path.Join(os.Getenv("HOME"), authorsFile), nil
+	return defaultAuthorsFile, nil
 }
 
 func getenvDefault(key, defaultValue string) (value string) {

test/git-duet.bats
@@ -192,10 +192,27 @@ load test_helper
   git duet -q dj fc
   run git duet
 
+  assert_success
   assert_line "GIT_AUTHOR_NAME='Dane Joe'"
   assert_line "GIT_AUTHOR_EMAIL='dane@bananas.biz.local'"
   assert_line "GIT_COMMITTER_NAME='Frances Car'"
   assert_line "GIT_COMMITTER_EMAIL='f.car@banana.info.local'"
+}
+
+@test "does not error when run outside of a git repository" {
+  run git duet -g jd fb
+  assert_success
+
+  mkdir ${GIT_DUET_TEST_DIR}/no-repo
+  cd ${GIT_DUET_TEST_DIR}/no-repo
+
+  unset GIT_DUET_AUTHORS_FILE
+  run git duet
+  assert_success
+  assert_line "GIT_AUTHOR_NAME='Jane Doe'"
+  assert_line "GIT_AUTHOR_EMAIL='jane@hamsters.biz.local'"
+  assert_line "GIT_COMMITTER_NAME='Frances Bar'"
+  assert_line "GIT_COMMITTER_EMAIL='f.bar@hamster.info.local'"
 }
 
 @test "installs prepare-commit-msg hook file if GIT_DUET_CO_AUTHORED_BY" {

test/git-solo.bats
@@ -177,6 +177,21 @@ load test_helper
   assert_success 'f.car@banana.info.local'
 }
 
+@test "does not error when run outside of a git repository" {
+  run git solo -g jd
+  assert_success
+
+  mkdir ${GIT_DUET_TEST_DIR}/no-repo
+  cd ${GIT_DUET_TEST_DIR}/no-repo
+
+  unset GIT_DUET_AUTHORS_FILE
+  git solo
+  run git solo
+  assert_success
+  assert_line "GIT_AUTHOR_NAME='Jane Doe'"
+  assert_line "GIT_AUTHOR_EMAIL='jane@hamsters.biz.local'"
+}
+
 @test "does not write Co-authored-by trailer if GIT_DUET_CO_AUTHORED_BY is set" {
   GIT_DUET_CO_AUTHORED_BY=1 git solo -q jd
   add_file first.txt